98a519c63fbc692c9094f0917714e392b91be557
Yanderson Kaio <yandersonkaio@gmail.com>
2025-02-05 12:33:36 -0300

    Configure user repositories

---
+++ Wallet.Core/Infrastructure/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Wallet.Core.Domain.Entities;
+
+namespace Wallet.Core.Infrastructure.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByEmail(string email);
+        Task Add(User user);
+        Task Update(User user);
+        Task Delete(Guid userId);
+    }
+}
---
+++ Wallet.Core/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wallet.Core.Domain.Entities;
+using Wallet.Core.Infrastructure.Database;
+using Wallet.Core.Infrastructure.Interfaces;
+
+namespace Wallet.Core.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly WalletDbContext _context;
+
+        public UserRepository(WalletDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetByEmail(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task Add(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
