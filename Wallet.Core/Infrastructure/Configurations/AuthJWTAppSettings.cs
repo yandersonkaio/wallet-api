18603a2bc63b66421f26c4d5cdf8f3203ed0c677
Yanderson Kaio <yandersonkaio@gmail.com>
2025-02-05 12:36:43 -0300

    Create appsettings related to jwt configuration

---
+++ Wallet.Core/Infrastructure/Configurations/AuthJWTAppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Wallet.Core.Infrastructure.Configurations
+{
+    public class AuthJWTAppSettings
+    {
+        public required string Key { get; set; }
+        public required string Issuer { get; set; }
+        public required string Audience { get; set; }
+    }
+}
