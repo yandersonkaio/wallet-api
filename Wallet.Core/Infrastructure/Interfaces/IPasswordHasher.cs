68f3e264a746b0f70475a961df4cc9770a64fa83
Yanderson Kaio <yandersonkaio@gmail.com>
2025-02-05 12:31:11 -0300

    Configure password hasher

---
+++ Wallet.Core/Infrastructure/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace Wallet.Core.Infrastructure.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+        bool Verify(string hashedPassword, string providedPassword);
+    }
+}
---
+++ Wallet.Core/Infrastructure/Services/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using Wallet.Core.Infrastructure.Interfaces;
+
+namespace Wallet.Core.Infrastructure.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string hashedPassword, string providedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(providedPassword, hashedPassword);
+        }
+    }
+}
