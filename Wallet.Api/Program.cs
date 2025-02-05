66233a16c8770a37e9bfd532dde7ea32c1ec347d
Yanderson Kaio <yandersonkaio@gmail.com>
2025-02-05 12:45:06 -0300

    Adjust program file

--- Wallet.Api/Program.cs
+++ Wallet.Api/Program.cs
@@ -1,5 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Wallet.Core.Infrastructure;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Wallet.Api.Middleware;
+using Wallet.Core.Application.Interfaces;
+using Wallet.Core.Application.UseCases.UserManagement;
+using Wallet.Core.Infrastructure.Configurations;
+using Wallet.Core.Infrastructure.Database;
+using Wallet.Core.Infrastructure.Interfaces;
+using Wallet.Core.Infrastructure.Repositories;
+using Wallet.Core.Infrastructure.Services;
 
 namespace Wallet
 {
@@ -9,17 +19,49 @@ namespace Wallet
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var jwtKey = builder.Configuration["Jwt:Key"];
+            var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+            var jwtAudience = builder.Configuration["Jwt:Audience"];
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+
             builder.Logging.ClearProviders();
             builder.Logging.AddConsole();
             builder.Logging.AddDebug();
 
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
-            );
+            builder.Services.AddDbContext<WalletDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("Wallet.Api")));
+
+            builder.Services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtIssuer,
+                        ValidAudience = jwtAudience,
+                        IssuerSigningKey = key
+                    };
+                });
+
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+            builder.Services.AddScoped<ICreateUserUseCase, CreateUserUseCase>();
+            builder.Services.AddScoped<IAuthenticateUserUseCase, AuthenticateUserUseCase>();
+            builder.Services.AddScoped<ITokenService, TokenService>();
+
+            builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddAuthorization();
+            builder.Services.Configure<AuthJWTAppSettings>(builder.Configuration.GetSection("Jwt"));
 
             var app = builder.Build();
 
@@ -29,7 +71,9 @@ namespace Wallet
                 app.UseSwaggerUI();
             }
 
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseAuthorization();
             app.MapControllers();
 
