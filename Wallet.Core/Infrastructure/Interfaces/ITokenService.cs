a27f508da101067f87eb2e558f5bbd6fc9bc6ef6
Yanderson Kaio <yandersonkaio@gmail.com>
2025-02-05 12:30:04 -0300

    Configure token jwt service

---
+++ Wallet.Core/Infrastructure/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using Wallet.Core.Application.DTOs.User;
+using Wallet.Core.Infrastructure.Configurations;
+
+namespace Wallet.Core.Infrastructure.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateToken(UserDto user, AuthJWTAppSettings settings);
+    }
+}
---
+++ Wallet.Core/Infrastructure/Services/TokenService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Wallet.Core.Application.DTOs.User;
+using Wallet.Core.Infrastructure.Configurations;
+using Wallet.Core.Infrastructure.Interfaces;
+
+namespace Wallet.Core.Infrastructure.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string GenerateToken(UserDto user, AuthJWTAppSettings settings)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.Key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: settings.Issuer,
+                audience: settings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
